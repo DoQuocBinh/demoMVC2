b22585a409978820b5d36d24ab0f92d8718f836f
DoQuocBinh <binhdq@fpt.edu.vn>
2022-01-20 10:44:02 +0700

    shopping cart

--- app.js
+++ app.js
@@ -20,6 +20,60 @@ app.get('/',requiresLogin,(req,res)=>{
     
 })
 
+app.get('/shopping',(req,res)=>{
+    res.render('showproducts')
+})
+
+app.post('/buy',(req,res)=>{
+    //xem nguoi dung mua gi: Milk hay Coffee
+    const product = req.body.product
+    //lay gio hang trong session
+    let cart = req.session["cart"]
+    //chua co gio hang trong session, day se la sp dau tien
+    if(!cart){
+        let dict = {}
+        dict[product] = 1
+        req.session["cart"] = dict
+        console.log("Ban da mua:" + product + ", so luong: " + dict[product])
+    }else{
+        dict = req.session["cart"]
+        //co lay product trong dict
+        var oldProduct = dict[product]
+        //kiem tra xem product da co trong Dict
+        if(!oldProduct)
+            dict[product] = 1
+        else{
+            dict[product] = parseInt(oldProduct) +1
+        }
+        req.session["cart"] = dict
+        console.log("Ban da mua:" + product + ", so luong: " + dict[product])
+    }
+
+    // if(!req.session[product])
+    //     req.session[product] = 1
+    // else{
+    //     const soLuong =  parseInt(req.session[product]) +1
+    //     req.session[product] = soLuong
+    // }
+    // console.log("Ban vua cap nhat sp" + product + " so luong: " + req.session[product])
+
+    res.render('showproducts')
+})
+
+app.get('/viewCart',(req,res)=>{
+    const cart = req.session["cart"]
+    //Mot array chua cac san pham trong gio hang
+    let spDaMua = []
+    //neu khach hang da mua it nhat 1 sp
+    if(cart){
+        const dict = req.session["cart"]
+        for(var key in dict) {
+            spDaMua.push({tensp: key,soLuong: dict[key]})
+         }
+    }
+    res.render('mycart',{products:spDaMua})
+})
+
 app.get('/login',(req,res)=>{
     res.render('loginpage')
 })
